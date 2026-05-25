a621281ee13ba967483d9ad8e034390000de39f6
ddjj55 <whye@tutamail.com>
2026-05-25 19:34:30 +0800

    Update post-details.js

--- js/post-details.js
+++ js/post-details.js
@@ -340,7 +340,7 @@ const handleClick = () => {
     videoElement.addEventListener('pause', () => {
         videoElement.style.cssText = `
             width: 120px;
-            height: 120px;
+            height: 180px;
             object-fit: cover;
             border-radius: 5px;
             display: block;
