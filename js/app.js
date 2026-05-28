ab887e18fe7200140840a411cf75ae87d6ea73eb
ddjj55 <whye@tutamail.com>
2026-05-28 16:52:32 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -111,7 +111,7 @@ showMoreBtn.style.cssText = `
     border-radius: 6px;
     cursor: pointer;
     font-size: 0.85rem;
-    color: #555;
+    color: #66B3FF;
     `;
 
     showMoreBtn.addEventListener('click', () => {
