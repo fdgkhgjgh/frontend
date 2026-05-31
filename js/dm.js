5061e2e3ffaec4917737f9c5e8d63caa20bcee87
ddjj55 <whye@tutamail.com>
2026-06-01 06:23:16 +0800

    Update dm.js

--- js/dm.js
+++ js/dm.js
@@ -213,7 +213,7 @@ document.getElementById('dm-messages').addEventListener('scroll', () => {
 const inputBar = document.getElementById('dm-input-bar');
 if (inputBar) inputBar.style.bottom = '0px';
     
-    document.getElementById('dm-messages').style.paddingBottom = '80px';
+    document.getElementById('dm-messages').style.paddingBottom = '60px';
     await loadDMMessages(true);
     subscribeDMChat();
 
